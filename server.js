424ccbedf96bd732dc27d3faed63013915713918
aajaber <aajaber.97@gmail.com>
2021-09-16 21:36:52 +0300

    lab07 update

--- server.js
+++ server.js
@@ -10,38 +10,27 @@ const PORT = process.env.PORT;
 
 const weather = require("./data/weather.json");
 
-
 class Forecast {
   constructor(date, description) {
     this.date = date;
     this.description = description;
-    Forecast.all.push(this);
   }
 }
-Forecast.all = [];
 
-app.get(
-  "/",
-  function (req, res) {
-    res.send("Hello World");
-  }
-);
-
-
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 app.get("/weather", (request, response) => {
-  //console.log(request);
-
   const city_name = request.query.city_name;
-  const lon = request.query.lon;
-  const lat = request.query.lat;
+  // const lon = request.query.lon;
+  // const lat = request.query.lat;
 
   if (city_name) {
     const returnArr = weather.find((item) => {
       return item.city_name.toLowerCase() === city_name.toLowerCase();
     });
     let dataArr = returnArr.data.map((value) => {
-
       return new Forecast(
         ` Low of ${value.low_temp}, high of ${value.high_temp} with ${value.weather.description} `,
         ` ${value.datetime}`
@@ -49,12 +38,11 @@ app.get("/weather", (request, response) => {
     });
     if (dataArr.length) {
       response.json(dataArr);
-      console.log(dataArr)
+      console.log(dataArr);
     } else {
       response.send("the city is not exists");
     }
   } else {
-
     response.json(weather);
   }
 });
